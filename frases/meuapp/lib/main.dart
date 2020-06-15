c56be6bc9a4fa2b3ef0646302a386d41d87bdbbd
Tiago-ts <tiagogatewayts@gmail.com>
2020-06-15 15:39:33 -0300

    att

--- frases/meuapp/lib/main.dart
+++ frases/meuapp/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MaterialApp(
@@ -15,6 +16,31 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  var _frases = [
+
+    "As pessoa não mudam quando chegam ao poder, elas se revelam!",
+    "Mantenha-se focado mesmo que as pessoas que você ama, estejam em outra sintonia.",
+    "Quando é para acontecer, até quem tenta te atrapalhar ajuda.",
+    "Não se distraia quando deveria estar focado, algumas distrações podem lhe custar caro.",
+    "Homens assumem os próprios erros, covardes procuram em quem por a culpa.",
+    "Não importa o quão leal você seja. Esteja preparado para a deslealdade dos outros."
+
+  ];
+
+  var _frasesGeradas = "Clique abaixo para gerar uma Frase!";
+
+  void _gerarFrase(){
+
+    var numeroSorteado = Random().nextInt(_frases.length);
+
+    setState(() {
+      _frasesGeradas = _frases[numeroSorteado];
+    });
+    print(numeroSorteado);
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,24 +50,30 @@ class _HomeState extends State<Home> {
       ),
 
 
-      body: Container(
+      body: Center(
+        child: Container(
+        //width: double.infinity,
+        
         padding: EdgeInsets.all(16),
-        decoration: BoxDecoration(
+       /* decoration: BoxDecoration(
           border: Border.all(width:3, color: Colors.red)
-        ),
+        ),*/
 
 
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children:<Widget>[
             Image.asset("imagens/logo.png"),
             Text(
-              "Clique abaixo para gerar uma frase!",
+              _frasesGeradas,
               textAlign: TextAlign.justify,
               style: TextStyle(
-                fontSize: 17,
+                fontSize: 20,
                 fontStyle: FontStyle.italic,
                 color: Colors.black
               ),
+
             ),
 
             RaisedButton(
@@ -55,13 +87,14 @@ class _HomeState extends State<Home> {
               ),),
              
               color: Colors.green,
-              onPressed: (){},
+              onPressed: _gerarFrase,
             )
           ]
         ),
 
 
       ),
+      )
 
 
     );
