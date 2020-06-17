0b13fca8bc6e7513ad4ba2effce1967562487968
Tiago-ts <tiagogatewayts@gmail.com>
2020-06-17 20:30:07 -0300

    interface

--- alcool_gasolina/lib/main.dart
+++ alcool_gasolina/lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:alcool_gasolina/Home.dart';
 
 
 
 void main() {
   runApp(MaterialApp(
-    home: Home(),
+    debugShowCheckedModeBanner: false,
+    home: Home(
+
+
+    ),
   ));
   
 }
