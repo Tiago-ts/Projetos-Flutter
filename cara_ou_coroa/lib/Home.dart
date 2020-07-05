6cb845a3073b1319f7b40921a737f16978b44cb8
Tiago-ts <tiagogatewayts@gmail.com>
2020-07-05 09:22:38 -0300

    imagens

---
+++ cara_ou_coroa/lib/Home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green,
+      //backgroundColor: Color(0xffffcc80),
+      //backgroundColor: Color.fromRGBO(255, 204, 128, 0.8),
+      body: Container(
+
+      ),
+    );
+  }
+}
--- cara_ou_coroa/lib/main.dart
+++ cara_ou_coroa/lib/main.dart
@@ -1,2 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:cara_ou_coroa/Home.dart';
 
+void main() => runApp(MaterialApp(
+  home: Home(),
+  debugShowCheckedModeBanner: false,
+));
