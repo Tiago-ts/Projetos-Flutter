9058323fa057b69b772eb190007354af6f506a88
Tiago-ts <tiagogatewayts@gmail.com>
2020-07-13 21:05:16 -0300

    Update Home.dart

--- consumo_servicos/lib/Home.dart
+++ consumo_servicos/lib/Home.dart
@@ -14,7 +14,28 @@ class _HomeState extends State<Home> {
 
   String _urlBase = "https://jsonplaceholder.typicode.com";
 
-  Future<List<Post>> _recuperarPost(){
+  Post post = Post(0, 0, "", "");
+
+  Future<List<Post>> _recuperarPost() async{
+
+  //  List<Post> lista = List();
+  //  lista.add(post);
+
+    http.Response response = await http.get(_urlBase + "/post");
+    var dadosJson = json.decode(response.body);
+    
+    List<Post> postagens = List();
+
+    for (var post in dadosJson ){
+      
+      print("post: " + post["title"]);
+      
+      Post p = Post(post["userId"], post["id"], post["title"], post["body"]);
+      postagens.add(p);
+      
+    }
+    return postagens;
+
 
   }
 
@@ -23,27 +44,21 @@ class _HomeState extends State<Home> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text("conumo de serviços"),
+        title: Text("Consumo de serviços"),
       ),
-      body: FutureBuilder<Map>(
+      
+      
+      body: FutureBuilder<List<Post>>(
 
-          future: null,
+          future: _recuperarPost(),
           builder: (context, snapshot){
-
-
+            
             switch(snapshot.connectionState){
 
-              case ConnectionState.done:
-                if(snapshot.hasError){
-
-                } else {
-
-                }
-
-                break;
-
               case ConnectionState.waiting:
-
+                return Center(
+                   child: CircularProgressIndicator(),
+                );
                 break;
 
               case ConnectionState.none:
@@ -51,12 +66,29 @@ class _HomeState extends State<Home> {
 
               case ConnectionState.active:
                 break;
+
+              case ConnectionState.done:
+                print("conexao done");
+                if(snapshot.hasError){
+                  print("lista: erro ao carregar");
+
+                } else {
+
+                  print("lista: carregou");
+
+                  return ListView.builder(
+                      itemCount: snapshot.data.length,
+                         itemBuilder: (context, index){
+
+                        return ListTile(
+                          title: Text("teste"),
+                          subtitle: Text("teste2"),
+                        );
+                      }
+                  );
+                }
+                break;
             }
-
-            return Center(
-              child: Text(""),
-            );
-
           },
       ),
     );
